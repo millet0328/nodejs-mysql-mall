b4bd7b1d661860efdaf9d3676cb0d50cca672df7
silky <nn880328@126.com>
2017-10-26 16:14:00 +0800

    商品数据

--- routes/admin.js
+++ routes/admin.js
@@ -103,10 +103,12 @@ router.get("/getCategory/sub", function(req, res) {
 
 //图片接口
 router.post("/upload/img", upload.single('file'), function(req, res) {
-	console.log(req.file);
+	//文件类型
+	var type = req.file.mimetype;
+	var size = req.file.size;
 	//判断是否为图片
-	var Exg = /^image\/\w+$/;
-	var flag = Exg.test(req.file.mimetype);
+	var reg = /^image\/\w+$/;
+	var flag = reg.test(type);
 	if(!flag) {
 		res.json({
 			status: false,
@@ -115,7 +117,7 @@ router.post("/upload/img", upload.single('file'), function(req, res) {
 		return;
 	}
 	//判断图片体积是否小于2M
-	if(req.file.size >= 2 * 1024 * 1024) {
+	if(size >= 2 * 1024 * 1024) {
 		res.json({
 			status: false,
 			msg: "图片体积太大，请压缩图片！"
@@ -131,7 +133,7 @@ router.post("/upload/img", upload.single('file'), function(req, res) {
 		});
 		return;
 	}
-	//处理图片文件名
+	//处理原文件名
 	var originalName = req.file.originalname;
 	var formate = originalName.split(".");
 	//扩展名
@@ -158,7 +160,111 @@ router.post("/upload/img", upload.single('file'), function(req, res) {
 		lgImg: fileFolder + filename + "_lg" + extName,
 		mdImg: fileFolder + filename + "_md" + extName
 	});
+});
 
+//轮播图上传API
+router.post("/upload/slideImg", upload.single('file'), function(req, res) {
+	//文件类型
+	var type = req.file.mimetype;
+	var size = req.file.size;
+	//判断是否为图片
+	var reg = /^image\/\w+$/;
+	var flag = reg.test(type);
+	if(!flag) {
+		res.json({
+			status: false,
+			msg: "格式错误，请选择一张图片！"
+		});
+		return;
+	}
+	//判断图片体积是否小于2M
+	if(size >= 2 * 1024 * 1024) {
+		res.json({
+			status: false,
+			msg: "图片体积太大，请压缩图片！"
+		});
+		return;
+	}
+	//判读图片尺寸
+	var width = images(req.file.buffer).width();
+	var height = images(req.file.buffer).height();
+	if(width != height) {
+		res.json({
+			status: false,
+			msg: "图片尺寸800*800，请重新上传!"
+		});
+		return;
+	}
+	if(width < 300 || width > 1500) {
+		res.json({
+			status: false,
+			msg: "图片尺寸300-1500，请重新处理!"
+		});
+		return;
+	}
+	//处理原文件名
+	var originalName = req.file.originalname;
+	var formate = originalName.split(".");
+	//扩展名
+	var extName = "." + formate[formate.length - 1];
+	var filename = uuidv1();
+	//储存文件夹
+	var fileFolder = "/images/goods/";
+	//处理图片
+	images(req.file.buffer)
+		.resize(720) //缩放尺寸至720宽
+		.save("public" + fileFolder + filename + "_lg" + extName, {
+			quality: 70 //保存图片到文件,图片质量为70
+		});
+	//返回储存结果
+	res.json({
+		status: true,
+		msg: "图片上传处理成功！",
+		slideImg: fileFolder + filename + "_lg" + extName
+	});
+});
+//通用图片上传API
+router.post("/upload/common", upload.single('file'), function(req, res) {
+	//文件类型
+	var type = req.file.mimetype;
+	var size = req.file.size;
+	//判断是否为图片
+	var reg = /^image\/\w+$/;
+	var flag = reg.test(type);
+	if(!flag) {
+		res.json({
+			errno: 1,
+			msg: "格式错误，请选择一张图片！"
+		});
+		return;
+	}
+	//判断图片体积是否小于2M
+	if(size >= 2 * 1024 * 1024) {
+		res.json({
+			errno: 1,
+			msg: "图片体积太大，请压缩图片！"
+		});
+		return;
+	}
+	//处理原文件名
+	var originalName = req.file.originalname;
+	var formate = originalName.split(".");
+	//扩展名
+	var extName = "." + formate[formate.length - 1];
+	var filename = uuidv1();
+	//储存文件夹
+	var fileFolder = "/images/details/";
+	//处理图片
+	images(req.file.buffer)
+		.save("public" + fileFolder + filename + extName, {
+			quality: 70 //保存图片到文件,图片质量为70
+		});
+	//返回储存结果
+	res.json({
+		errno: 0,
+		msg: "图片上传处理成功！",
+		data: [fileFolder + filename + extName]
+	});
 });
 
 module.exports = router;
