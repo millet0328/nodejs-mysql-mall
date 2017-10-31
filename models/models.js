de9e8141323e8f65d584e4e1c897010f755f62be
silky <nn880328@126.com>
2017-10-31 22:01:01 +0800

    更新

--- models/models.js
+++ models/models.js
@@ -5,20 +5,20 @@ var models = {};
 
 // 账户结构
 var userSchema = mongoose.Schema({
-	account: String,
-	password: String,
-	nickname: {
-		type: String,
-		default: ''
-	},
-	sex: {
-		type: String,
-		default: '男'
-	},
-	avatar: {
-		type: String,
-		default: 'img/userlogo.png'
-	}
+    account: String,
+    password: String,
+    nickname: {
+        type: String,
+        default: ''
+    },
+    sex: {
+        type: String,
+        default: '男'
+    },
+    avatar: {
+        type: String,
+        default: 'img/userlogo.png'
+    }
 });
 
 // 大写代表model
@@ -26,30 +26,30 @@ models.User = mongoose.model('User', userSchema);
 
 //收货地址
 var addressSchema = mongoose.Schema({
-	uid: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "User"
-	},
-	name: String,
-	tel: String,
-	city: String,
-	street: String,
-	isDefault: Boolean
+    uid: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    name: String,
+    tel: String,
+    city: String,
+    street: String,
+    isDefault: Boolean
 });
 // model化
 models.Address = mongoose.model('Address', addressSchema);
 
 //商品分类结构
 var categorySchema = mongoose.Schema({
-	id: String,
-	level: Number,
-	name: String,
-	pId: {
-		type: String,
-		default: "0"
-	},
-	parentTId: String,
-	tId: String
+    id: String,
+    level: Number,
+    name: String,
+    pId: {
+        type: String,
+        default: "0"
+    },
+    parentTId: String,
+    tId: String
 });
 
 // 大写代表model
@@ -57,24 +57,26 @@ models.Category = mongoose.model('Category', categorySchema);
 
 // 商品结构
 var goodsSchema = mongoose.Schema({
-	cateFirst: String,
-	cateSecond: String,
-	cateThird: String,
-	name: String,
-	hotPoint: String,
-	price: Number,
-	marketPrice: Number,
-	cost: Number,
-	discount: String,
-	inventory: Number,
-	articleNo: Number,
-	lgImg: String,
-	mdImg: String,
-	slideImgs: [String],
-	brand: String,
-	detail: String,
-	local: String,
-	freight: Number
+    cateFirst: String,
+    cateSecond: String,
+    cateThird: String,
+    name: String,
+    hotPoint: String,
+    price: Number,
+    marketPrice: Number,
+    cost: Number,
+    discount: String,
+    inventory: Number,
+    articleNo: Number,
+    lgImg: String,
+    mdImg: String,
+    slideImgs: [String],
+    brand: String,
+    detail: String,
+    Province: String,
+    City: String,
+    Area: String,
+    freight: Number
 });
 
 // 大写代表model
