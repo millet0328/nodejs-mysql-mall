160135ba9d06dba73855bd6b3d0d308b6e621b05
silky <nn880328@126.com>
2019-05-13 19:20:14 +0800

    获取用户列表，返回手机号，id

--- routes/users.js
+++ routes/users.js
@@ -41,35 +41,37 @@ let db = require('../config/mysql');
  * @apiSampleRequest /api/user/register
  */
 router.post('/user/register/', function(req, res) {
-    // 查询账户是否存在
-    let sql = `SELECT * FROM USERS WHERE username = ?`
-    db.query(sql, [req.body.username], function(results, fields) {
-        if (results.length) {
-            res.json({
-                status: false,
-                msg: "账号已经存在！"
-            });
-            return false;
+  // 查询账户是否存在
+  let sql = `SELECT * FROM USERS WHERE username = ?`
+  db.query(sql, [req.body.username], function(results, fields) {
+    if (results.length) {
+      res.json({
+        status: false,
+        msg: "账号已经存在！"
+      });
+      return false;
+    }
+    let sql = `INSERT INTO USERS (username,password,nickname,sex,tel,create_time) VALUES (?,?,?,?,?,CURRENT_TIMESTAMP())`
+    db.query(sql, [req.body.username, req.body.password, req.body.nickname, req.body.sex, req.body.tel], function(results, fields) {
+      let payload = {
+        id: results.insertId,
+        username: req.body.username
+      }
+      // 生成token
+      let token = jwt.sign(payload, 'secret', {
+        expiresIn: '2h'
+      });
+      // 存储成功
+      res.json({
+        status: true,
+        msg: "注册成功！",
+        data: {
+          token,
+          id: results.insertId,
         }
-        let sql = `INSERT INTO USERS (username,password,nickname,sex,tel,create_time) VALUES (?,?,?,?,?,CURRENT_TIMESTAMP())`
-        db.query(sql, [req.body.username, req.body.password, req.body.nickname, req.body.sex, req.body.tel], function(results, fields) {
-            let payload = {
-                id: results.insertId,
-                username: req.body.username
-            }
-            // 生成token
-            let token = jwt.sign(payload, 'secret', { expiresIn: '2h' });
-            // 存储成功
-            res.json({
-                status: true,
-                msg: "注册成功！",
-                data: {
-                    token,
-                    id: results.insertId,
-                }
-            });
-        })
-    });
+      });
+    })
+  });
 });
 
 /**
@@ -87,39 +89,41 @@ router.post('/user/register/', function(req, res) {
  */
 
 router.post('/user/login/', function(req, res) {
-    let sql = `SELECT * FROM USERS WHERE username = ? AND password = ? `;
-    db.query(sql, [req.body.username, req.body.password], function(results) {
-        // 账号密码错误
-        if (!results.length) {
-            res.json({
-                status: false,
-                msg: "账号或者密码错误！"
-            });
-            return false;
+  let sql = `SELECT * FROM USERS WHERE username = ? AND password = ? `;
+  db.query(sql, [req.body.username, req.body.password], function(results) {
+    // 账号密码错误
+    if (!results.length) {
+      res.json({
+        status: false,
+        msg: "账号或者密码错误！"
+      });
+      return false;
+    }
+    // 更新登陆时间，登陆次数
+    let sql = `UPDATE users SET login_count = login_count + 1 WHERE id = ?;`
+    db.query(sql, [results[0].id], function(response) {
+      if (response.affectedRows > 0) {
+        // 登录成功
+        let payload = {
+          id: results[0].id,
+          username: req.body.username
         }
-        // 更新登陆时间，登陆次数
-        let sql = `UPDATE users SET login_count = login_count + 1 WHERE id = ?;`
-        db.query(sql, [results[0].id], function(response) {
-            if (response.affectedRows > 0) {
-                // 登录成功
-                let payload = {
-                    id: results[0].id,
-                    username: req.body.username
-                }
-                // 生成token
-                let token = jwt.sign(payload, 'secret', { expiresIn: '2h' });
-                res.json({
-                    status: true,
-                    msg: "登录成功！",
-                    data: {
-                        token,
-                        id: results[0].id,
-                    }
-                });
-            }
+        // 生成token
+        let token = jwt.sign(payload, 'secret', {
+          expiresIn: '2h'
         });
-
+        res.json({
+          status: true,
+          msg: "登录成功！",
+          data: {
+            token,
+            id: results[0].id,
+          }
+        });
+      }
     });
+
+  });
 });
 /**
  * @api {get} /api/user/list/ 获取用户列表
@@ -130,23 +134,23 @@ router.post('/user/login/', function(req, res) {
  * @apiSampleRequest /api/user/list
  */
 router.get("/user/list/", function(req, res) {
-    //查询账户数据
-    let sql = `SELECT username,nickname,sex,avatar FROM USERS`;
-    db.query(sql, [], function(results, fields) {
-        if (!results.length) {
-            res.json({
-                status: false,
-                msg: "获取失败！"
-            });
-            return false;
-        }
-        // 获取成功
-        res.json({
-            status: true,
-            msg: "获取成功！",
-            data: results
-        });
-    })
+  //查询账户数据
+  let sql = `SELECT id,username,nickname,sex,avatar,tel FROM USERS`;
+  db.query(sql, [], function(results, fields) {
+    if (!results.length) {
+      res.json({
+        status: false,
+        msg: "获取失败！"
+      });
+      return false;
+    }
+    // 获取成功
+    res.json({
+      status: true,
+      msg: "获取成功！",
+      data: results
+    });
+  })
 });
 /**
  * @api {get} /api/user/info/ 获取个人资料
@@ -158,23 +162,23 @@ router.get("/user/list/", function(req, res) {
  * @apiSampleRequest /api/user/info
  */
 router.get("/user/info/", function(req, res) {
-    //查询账户数据
-    let sql = `SELECT username,nickname,sex,avatar FROM USERS WHERE id = ?`;
-    db.query(sql, [req.query.uid], function(results, fields) {
-        if (!results.length) {
-            res.json({
-                status: false,
-                msg: "获取失败！"
-            });
-            return false;
-        }
-        // 获取成功
-        res.json({
-            status: true,
-            msg: "获取成功！",
-            data: results[0]
-        });
-    })
+  //查询账户数据
+  let sql = `SELECT username,nickname,sex,avatar FROM USERS WHERE id = ?`;
+  db.query(sql, [req.query.uid], function(results, fields) {
+    if (!results.length) {
+      res.json({
+        status: false,
+        msg: "获取失败！"
+      });
+      return false;
+    }
+    // 获取成功
+    res.json({
+      status: true,
+      msg: "获取成功！",
+      data: results[0]
+    });
+  })
 });
 /**
  * @api { post } /api/user/info/update/ 更新个人资料
@@ -189,13 +193,13 @@ router.get("/user/info/", function(req, res) {
  * @apiSampleRequest /api/user/info/update/
  */
 router.post("/user/info/update/", function(req, res) {
-    let sql = `UPDATE users SET nickname = ?,sex = ?,avatar = ? WHERE id = ?`
-    db.query(sql, [req.body.nickname, req.body.sex, req.body.avatar, req.body.uid], function(results, fields) {
-        res.json({
-            status: true,
-            msg: "修改成功！"
-        });
+  let sql = `UPDATE users SET nickname = ?,sex = ?,avatar = ? WHERE id = ?`
+  db.query(sql, [req.body.nickname, req.body.sex, req.body.avatar, req.body.uid], function(results, fields) {
+    res.json({
+      status: true,
+      msg: "修改成功！"
     });
+  });
 });
 
 /**
@@ -216,25 +220,24 @@ router.post("/user/info/update/", function(req, res) {
  * @apiSampleRequest /api/address/add/
  */
 router.post('/address/add', function(req, res) {
-    let sql;
-    let isDefault = req.body.isDefault;
-    if (isDefault == '1') {
-        sql =
-            `
-		UPDATE addresses SET isDefault = 0 WHERE uid = ${req.body.uid};
-		INSERT INTO addresses(uid,name,tel,province,city,area,street,code,isDefault) VALUES(?,?,?,?,?,?,?,?,?);
-		`
-    } else {
-        sql = `INSERT INTO addresses(uid,name,tel,province,city,area,street,code,isDefault) VALUES(?,?,?,?,?,?,?,?,?)`
-    }
-    db.query(sql, [req.body.uid, req.body.name, req.body.tel, req.body.province, req.body.city, req.body.area, req.body.street,
-        req.body.code, req.body.isDefault
-    ], function(results, fields) {
-        res.json({
-            status: true,
-            msg: "添加成功！"
-        });
+  let sql;
+  let isDefault = req.body.isDefault;
+  if (isDefault == '1') {
+    sql = `
+        UPDATE addresses SET isDefault = 0 WHERE uid = ${req.body.uid};
+        INSERT INTO addresses(uid,name,tel,province,city,area,street,code,isDefault) VALUES(?,?,?,?,?,?,?,?,?);
+        `
+  } else {
+    sql = `INSERT INTO addresses(uid,name,tel,province,city,area,street,code,isDefault) VALUES(?,?,?,?,?,?,?,?,?)`
+  }
+  db.query(sql, [req.body.uid, req.body.name, req.body.tel, req.body.province, req.body.city, req.body.area, req.body.street,
+    req.body.code, req.body.isDefault
+  ], function(results, fields) {
+    res.json({
+      status: true,
+      msg: "添加成功！"
     });
+  });
 });
 /**
  * @api {post} /api/address/delete/ 删除收货地址
@@ -246,14 +249,14 @@ router.post('/address/add', function(req, res) {
  * @apiSampleRequest /api/address/delete/
  */
 router.post("/address/delete/", function(req, res) {
-    var sql = `DELETE FROM addresses WHERE id = ? `
-    db.query(sql, [req.body.id], function(results, fields) {
-        res.json({
-            status: true,
-            data: results,
-            msg: "删除成功！"
-        });
-    })
+  var sql = `DELETE FROM addresses WHERE id = ? `
+  db.query(sql, [req.body.id], function(results, fields) {
+    res.json({
+      status: true,
+      data: results,
+      msg: "删除成功！"
+    });
+  })
 })
 /**
  * @api {post} /api/address/update/ 修改收货地址
@@ -274,26 +277,24 @@ router.post("/address/delete/", function(req, res) {
  * @apiSampleRequest /api/address/update/
  */
 router.post("/address/update/", function(req, res) {
-    let sql;
-    let isDefault = req.body.isDefault;
-    if (isDefault == '1') {
-        sql =
-            `
-		UPDATE addresses SET isDefault = 0 WHERE uid = ${req.body.uid};
-		UPDATE addresses SET uid = ?,name = ?,tel = ?,province = ?,city = ?,area = ?,street = ?,code = ?,isDefault = ? WHERE id = ?;
-		`
-    } else {
-        sql =
-            `UPDATE addresses SET uid = ?,name = ?,tel = ?,province = ?,city = ?,area = ?,street = ?,code = ?,isDefault = ? WHERE id = ?`
-    }
-    db.query(sql, [req.body.uid, req.body.name, req.body.tel, req.body.province, req.body.city, req.body.area, req.body.street,
-        req.body.code, req.body.isDefault, req.body.id
-    ], function(results, fields) {
-        res.json({
-            status: true,
-            msg: "修改成功！"
-        });
+  let sql;
+  let isDefault = req.body.isDefault;
+  if (isDefault == '1') {
+    sql = `
+        UPDATE addresses SET isDefault = 0 WHERE uid = ${req.body.uid};
+        UPDATE addresses SET uid = ?,name = ?,tel = ?,province = ?,city = ?,area = ?,street = ?,code = ?,isDefault = ? WHERE id = ?;
+        `
+  } else {
+    sql = `UPDATE addresses SET uid = ?,name = ?,tel = ?,province = ?,city = ?,area = ?,street = ?,code = ?,isDefault = ? WHERE id = ?`
+  }
+  db.query(sql, [req.body.uid, req.body.name, req.body.tel, req.body.province, req.body.city, req.body.area, req.body.street,
+    req.body.code, req.body.isDefault, req.body.id
+  ], function(results, fields) {
+    res.json({
+      status: true,
+      msg: "修改成功！"
     });
+  });
 })
 /**
  * @api {get} /api/address/list/ 获取收货地址列表
@@ -305,21 +306,21 @@ router.post("/address/update/", function(req, res) {
  * @apiSampleRequest /api/address/list/
  */
 router.get('/address/list', function(req, res) {
-    var sql = `SELECT * FROM addresses WHERE uid = ? `
-    db.query(sql, [req.query.uid], function(results, fields) {
-        if (!results.length) {
-            res.json({
-                status: false,
-                msg: "暂无收货地址！"
-            });
-            return false;
-        }
-        res.json({
-            status: true,
-            data: results,
-            msg: "获取成功！"
-        });
-    })
+  var sql = `SELECT * FROM addresses WHERE uid = ? `
+  db.query(sql, [req.query.uid], function(results, fields) {
+    if (!results.length) {
+      res.json({
+        status: false,
+        msg: "暂无收货地址！"
+      });
+      return false;
+    }
+    res.json({
+      status: true,
+      data: results,
+      msg: "获取成功！"
+    });
+  })
 });
 /**
  * @api {get} /api/address/detail/ 根据id获取收货地址详情
@@ -331,21 +332,21 @@ router.get('/address/list', function(req, res) {
  * @apiSampleRequest /api/address/detail/
  */
 router.get("/address/detail/", function(req, res) {
-    var sql = `SELECT * FROM addresses WHERE id = ? `
-    db.query(sql, [req.query.id], function(results, fields) {
-        if (!results.length) {
-            res.json({
-                status: false,
-                msg: "暂无收货地址信息！"
-            });
-            return false;
-        }
-        res.json({
-            status: true,
-            data: results[0],
-            msg: "获取成功！"
-        });
-    })
+  var sql = `SELECT * FROM addresses WHERE id = ? `
+  db.query(sql, [req.query.id], function(results, fields) {
+    if (!results.length) {
+      res.json({
+        status: false,
+        msg: "暂无收货地址信息！"
+      });
+      return false;
+    }
+    res.json({
+      status: true,
+      data: results[0],
+      msg: "获取成功！"
+    });
+  })
 })
 
 module.exports = router;
