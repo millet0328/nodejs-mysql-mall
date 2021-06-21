3c8ae0207170272d2a69174b4042000b439690b5
鞠祥明 <nn880328@126.com>
2021-06-21 10:21:18 +0800

    修复注册用户默认头像地址路径错误

--- routes/mall/user.js
+++ routes/mall/user.js
@@ -33,7 +33,6 @@ let db = require('../../config/mysql');
  * 
  * @apiParam {String} username 用户账户名.
  * @apiParam {String} password 用户密码.
- * @apiParam {String} nickname 用户昵称.
  * @apiParam { String } sex 性别.
  * @apiParam { String } tel 手机号码.
  * 
@@ -41,36 +40,38 @@ let db = require('../../config/mysql');
  * 
  * @apiSampleRequest /api/user/register
  */
-router.post('/register', function (req, res) {
-    let { username, password, nickname, sex, tel } = req.body;
-    // 查询账户是否存在
-    let sql = `SELECT * FROM user WHERE username = ?`
-    db.query(sql, [username], function (results) {
-        if (results.length) {
-            res.json({
-                status: false,
-                msg: "账号已经存在！"
-            });
-            return false;
-        }
-        let sql = `INSERT INTO user (username,password,nickname,sex,tel,create_time) VALUES (?,?,?,?,?,CURRENT_TIMESTAMP())`;
-        db.query(sql, [username, password, nickname, sex, tel], function (results) {
-            let { insertId } = results;
-            // 生成token
-            let token = jwt.sign({ id: insertId, }, 'secret', {
-                expiresIn: '4h'
-            });
-            // 存储成功
-            res.json({
-                status: true,
-                msg: "注册成功！",
-                data: {
-                    token,
-                    id: insertId,
-                }
-            });
-        });
-    });
+router.post('/register', function(req, res) {
+	let { username, password, sex, tel } = req.body;
+	// 查询账户是否存在
+	let sql = `SELECT * FROM user WHERE username = ?`
+	db.query(sql, [username], function(results) {
+		if (results.length) {
+			res.json({
+				status: false,
+				msg: "账号已经存在！"
+			});
+			return false;
+		}
+		let defaultAvatar = process.env.server + '/images/avatar/default.jpg';
+		let sql =
+			`INSERT INTO user (username,password,nickname,sex,avatar,tel,create_time) VALUES (?,?,?,?,?,CURRENT_TIMESTAMP())`;
+		db.query(sql, [username, password, username, sex, defaultAvatar, tel], function(results) {
+			let { insertId } = results;
+			// 生成token
+			let token = jwt.sign({ id: insertId, }, 'secret', {
+				expiresIn: '4h'
+			});
+			// 存储成功
+			res.json({
+				status: true,
+				msg: "注册成功！",
+				data: {
+					token,
+					id: insertId,
+				}
+			});
+		});
+	});
 });
 
 /**
@@ -88,32 +89,32 @@ router.post('/register', function (req, res) {
  * @apiSampleRequest /api/user/login
  */
 
-router.post('/login', function (req, res) {
-    let { username, password } = req.body;
-    let sql = `SELECT * FROM user WHERE username = ? AND password = ?`;
-    db.query(sql, [username, password], function (results) {
-        // 账号密码错误
-        if (!results.length) {
-            res.json({
-                status: false,
-                msg: "账号或者密码错误！"
-            });
-            return false;
-        }
-        let { id } = results[0];
-        // 登录成功,生成token
-        let token = jwt.sign({ id }, 'secret', {
-            expiresIn: '4h'
-        });
-        res.json({
-            status: true,
-            msg: "登录成功！",
-            data: {
-                token,
-                id,
-            }
-        });
-    });
+router.post('/login', function(req, res) {
+	let { username, password } = req.body;
+	let sql = `SELECT * FROM user WHERE username = ? AND password = ?`;
+	db.query(sql, [username, password], function(results) {
+		// 账号密码错误
+		if (!results.length) {
+			res.json({
+				status: false,
+				msg: "账号或者密码错误！"
+			});
+			return false;
+		}
+		let { id } = results[0];
+		// 登录成功,生成token
+		let token = jwt.sign({ id }, 'secret', {
+			expiresIn: '4h'
+		});
+		res.json({
+			status: true,
+			msg: "登录成功！",
+			data: {
+				token,
+				id,
+			}
+		});
+	});
 });
 
 /**
@@ -124,18 +125,18 @@ router.post('/login', function (req, res) {
  * 
  * @apiSampleRequest /api/user/info
  */
-router.get("/info", function (req, res) {
-    let { id } = req.user;
-    //查询账户数据
-    let sql = `SELECT id,username,nickname,sex,avatar,tel FROM user WHERE id = ?`;
-    db.query(sql, [id], function (results) {
-        // 获取成功
-        res.json({
-            status: true,
-            msg: "获取成功！",
-            data: results[0]
-        });
-    })
+router.get("/info", function(req, res) {
+	let { id } = req.user;
+	//查询账户数据
+	let sql = `SELECT id,username,nickname,sex,avatar,tel FROM user WHERE id = ?`;
+	db.query(sql, [id], function(results) {
+		// 获取成功
+		res.json({
+			status: true,
+			msg: "获取成功！",
+			data: results[0]
+		});
+	})
 });
 
 /**
@@ -151,16 +152,16 @@ router.get("/info", function (req, res) {
  * 
  * @apiSampleRequest /api/user/edit
  */
-router.post("/edit", function (req, res) {
-    let { nickname, sex, avatar, tel } = req.body;
-    let { id } = req.user;
-    let sql = `UPDATE user SET nickname = ?,sex = ?,avatar = ? ,tel = ? WHERE id = ?`;
-    db.query(sql, [nickname, sex, avatar, tel, id], function (results) {
-        res.json({
-            status: true,
-            msg: "修改成功！"
-        });
-    });
+router.post("/edit", function(req, res) {
+	let { nickname, sex, avatar, tel } = req.body;
+	let { id } = req.user;
+	let sql = `UPDATE user SET nickname = ?,sex = ?,avatar = ? ,tel = ? WHERE id = ?`;
+	db.query(sql, [nickname, sex, avatar, tel, id], function(results) {
+		res.json({
+			status: true,
+			msg: "修改成功！"
+		});
+	});
 });
 
 module.exports = router;
